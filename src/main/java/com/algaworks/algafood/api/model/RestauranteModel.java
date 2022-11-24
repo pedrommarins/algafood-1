35cc48d100d1152b6c146521b0013a2fdd6f27ef
Pedro <pdpl1993@gmail.com>
2022-11-24 09:05:53 +0000

    Configurando DTOs

--- src/main/java/com/algaworks/algafood/api/model/RestauranteModel.java
+++ src/main/java/com/algaworks/algafood/api/model/RestauranteModel.java
@@ -5,19 +5,15 @@ import java.math.BigDecimal;
 import lombok.Getter;
 import lombok.Setter;
 
-@Getter
 @Setter
+@Getter
 public class RestauranteModel {
-	
-	private Long id;
-	
-	private String nome;
-	
-	private BigDecimal taxaFrete;
-	
-	private CozinhaModel cozinha;
-	
-	private Boolean ativo;
-	
 
+	private Long id;
+	private String nome;
+	private BigDecimal taxaFrete;
+	private CozinhaModel cozinha;
+	private Boolean ativo;
+	private EnderecoModel endereco;
+	
 }
